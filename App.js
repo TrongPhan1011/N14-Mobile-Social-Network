9d0a85b87b93b2e7d224f59e01236005fd294b40
TrongPhan1011 <huutrong2k1@gmail.com>
2022-09-02 09:14:07 +0700

    fix app.js

--- App.js
+++ App.js
@@ -1,29 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
-import { Fragment } from 'react';
 
 import DefaultLayout from './layout/DefaultLayout';
-import { publicScreen } from './configScreen';
+import ChatScreen from './screens/ChatScreen';
 
 export default function App() {
     return (
         <>
-            {publicScreen.map((screenLayout, index) => {
-                let Layout = DefaultLayout;
-                if (screenLayout.layout) Layout = screenLayout.layout;
-                else if (screenLayout.layout === null) {
-                    Layout = Fragment;
-                }
-
-                var Screen = screenLayout.screen;
-                return (
-                    <View key={index}>
-                        <Layout>
-                            <Screen />
-                        </Layout>
-                    </View>
-                );
-            })}
+            <DefaultLayout>
+                <ChatScreen />
+            </DefaultLayout>
 
             <StatusBar style="auto" />
         </>
